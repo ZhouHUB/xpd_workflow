936b17530decb209f343c8136d45c1826e6d7c20
CJ-Wright <cjwright4242@gmail.com>
2016-03-29 10:53:09 -0400

    WIP: finish the workflow to produce masked integrated products

--- examples/integration_workflow.py
+++ examples/integration_workflow.py
@@ -8,8 +8,12 @@ from xpd_workflow.mask_tools import *
 hdrs = []
 for hdr in hdrs:
     time_dept_bg = True
+
+    # Get calibrations
     geo1 = retrieve(hdr['calibration_file1'])
     geo2 = retrieve(hdr['calibration_file2'])
+
+    # Get starting masks
     start_mask1 = retrieve(hdr['start_mask1'])
     start_mask2 = retrieve(hdr['start_mask2'])
     for event in get_events(hdr):
@@ -18,8 +22,9 @@ for hdr in hdrs:
         q = geo.qArray(img.shape)
         fimg = img.flatten()
 
-        msk0 = mask_beamstop(geo, 1.)
+        # Needs to work on a real 2D image
         msk1 = mask_edge(img, 10)
+        msk0 = mask_beamstop(geo, 1.)
         msk2 = mask_radial_edge(img, geo, 31)
         initial_mask = msk0 | msk1 | msk2 | start_mask
         tmsk = msk0 | msk1 | msk2 | start_mask
@@ -51,3 +56,12 @@ for hdr in hdrs:
                                    range=[0, q.max()], statistic=np.std)
 
         # Post masking data
+        mr = dc(r)
+        mr[tmsk.astype(np.bool)] = -1
+
+        msk_median = sts.binned_statistic(mr.ravel(), img.ravel(), bins=bins,
+                                          range=[0, mr.max()], statistic='median')
+        msk_mean = sts.binned_statistic(mr.ravel(), img.ravel(), bins=bins,
+                                        range=[0, mr.max()], statistic='mean')
+        msk_std = sts.binned_statistic(mr.ravel(), img.ravel(), bins=bins,
+                                       range=[0, mr.max()], statistic=np.std)
