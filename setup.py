64d6304a7d6268a593f0a8aab84b717322eec77d
CJ-Wright <cjwright4242@gmail.com>
2016-03-30 16:21:42 -0400

    ENH: working integration workflow and setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='xpd_workflow',
+    version='',
+    packages=['xpd_workflow', 'xpd_workflow.tests'],
+    url='',
+    license='',
+    author='Christopher J. Wright',
+    author_email='',
+    description=''
+)
